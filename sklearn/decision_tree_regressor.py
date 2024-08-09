3f832f4395f0bf29e50983baa94c829fdd272943
River Ludington <ludingtonr@gmail.com>
2024-08-09 09:43:52 -0600

    feat: added tuning on max_leaf_node param

--- sklearn/decision_tree_regressor.py
+++ sklearn/decision_tree_regressor.py
@@ -3,6 +3,69 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
+def decision_tree_regressor(features, target, max_leaf_nodes) -> tuple[DecisionTreeRegressor, int]:
+    """
+    This function takes the feature data, target data, and a list of max_leaf_node integers,
+    and it trains a DecisionTreeRegressor model. The function will return the model, and it's MAE.
+
+    Args:
+        features: Pandas Dataframe
+        target: Pandas Dataframe
+        max_leaf_nodes: int
+    Returns:
+        DecisionTreeRegressor model, Mean Absolue Error (MAE)
+    """
+
+    # Train/Test split the data
+    train_X, val_X, train_y, val_y = train_test_split(features, target, random_state=0)
+
+    # Create a model
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=1)
+
+    # Fit the model
+    model.fit(train_X, train_y)
+
+    # Make predictions
+    predictions = model.predict(val_X)
+
+    # Calculate MAE
+    mae = mean_absolute_error(val_y, predictions)
+    
+    return model, mae
+
+
+def leaf_node_tuner(features, target, max_leaf_node_list) -> DecisionTreeRegressor:
+    """
+    This function takes the feature data, target data, and a list of max_leaf_node integers,
+    and it trains a DecisionTreeRegressor model with each max_leaf_node integer param. 
+    The function will then print the MAE, and return the model with the best performance.
+
+    Args:
+        features: Pandas Dataframe
+        target: Pandas Dataframe
+        max_leaf_node_list: List of integers
+    Returns:
+        DecisionTreeRegressor model
+    """
+
+    # Test all leaf_node_list integers
+    results_dict = {}
+    for leaf in max_leaf_node_list:
+        model, mae = decision_tree_regressor(features, target, leaf)
+        results_dict[mae] = model
+        print(f"A max_leaf_count of {leaf} produced MAE {mae}")
+
+    # Grab the lowest MAE
+    mae_list = list(results_dict.keys())
+    best_mae = min(mae_list)
+    best_model = results_dict[best_mae]
+
+    return best_model
+
+
+### EXAMPLE ###
+
+
 # Grab the Data from CSV
 melbourne_data = pd.read_csv('./data/melb_data.csv')
 
@@ -16,18 +79,6 @@ X = melbourne_data[melbourne_features]
 # Select the target
 y = melbourne_data['Price']
 
-# Train/Test split the data
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
-
-# Create a model
-melbourne_model = DecisionTreeRegressor(random_state=1)
-
-# Fit the model
-melbourne_model.fit(train_X, train_y)
-
-# Make predictions
-predictions = melbourne_model.predict(val_X)
-
-# Calculate MAE
-mae = mean_absolute_error(val_y, predictions)
-print(f'Mean Absolute Error: {mae}')
+# Test a few max_leaf_node integer options to tune and find the best model
+max_leaf_node_list = [5, 50, 500, 5000]
+model = leaf_node_tuner(features=X, target=y, max_leaf_node_list=max_leaf_node_list)
